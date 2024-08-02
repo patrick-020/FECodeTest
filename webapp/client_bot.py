9e26bcbe7038a73f0f8ff693acc52c3e0f138c76
patrick-020 <patrickanddev@outlook.com>
2024-08-02 22:10:33 +0800

    Init code commit on client_bot.py

--- webapp/client_bot.py
+++ webapp/client_bot.py
@@ -1,1 +1,24 @@
+from websockets.sync.client import connect
+import random, time
+
+
+def hello():
+    """
+    This is a chatbot that sends "Hello World!" appends with a counter every second
+    """
+    try:
+        counter: int = 1
+        with connect("ws://localhost:8000/ws/bot" + str(random.randint(0, 100))) as websocket:
+            while True:
+                websocket.send(f"Hello world! {counter}")
+                message = websocket.recv()
+                print(f"{message}", flush=True)
+                time.sleep(1)
+                counter += 1
+    except KeyboardInterrupt:
+        print(f"\nChatbot has left the chat, Ciao.")
+
+
+if __name__ == '__main__':
+    hello()
 
